13c532b35829a80a71ea79a55deaba1efb0e8dca
Will Zhou <iwillzhou@outlook.com>
2024-12-16 01:56:49 +0800

    feat: fix layout inconsistencies between iOS and Android

--- src/app/(app)/(tabs)/_layout.tsx
+++ src/app/(app)/(tabs)/_layout.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
+import { Platform } from 'react-native';
+import { useNavigationTheme } from 'src/hooks/theme';
+import { PlatformPressable } from '@react-navigation/elements';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { HomeStroke, HomeSolid, UserStroke, UserSolid } from 'src/components/icons';
 
 export default function TabsLayout() {
+    const insets = useSafeAreaInsets();
+    const navTheme = useNavigationTheme();
+
     return (
-        <Tabs screenOptions={{ headerTitleAlign: 'center' }}>
+        <Tabs
+            screenOptions={{
+                headerTitleAlign: 'center',
+                headerTitleStyle: {
+                    fontSize: 16
+                },
+                headerStyle: {
+                    height: (Platform.OS === 'android' ? 56 : 38) + insets.top
+                },
+                tabBarButton: props => <PlatformPressable {...props} pressColor={navTheme.colors.background} />
+            }}
+        >
             <Tabs.Screen
                 name="index"
                 options={{
                     title: 'Home',
-                    tabBarIcon: ({ color, focused }) => {
-                        return focused ? <HomeSolid color={color} /> : <HomeStroke color={color} />;
-                    }
+                    tabBarIcon: ({ color, focused }) =>
+                        focused ? <HomeSolid color={color} /> : <HomeStroke color={color} />
                 }}
             />
             <Tabs.Screen
--- src/app/(app)/settings/_layout.tsx
+++ src/app/(app)/settings/_layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Text } from 'src/components/ui';
+import { Button } from 'src/components/ui';
 import { Stack, router } from 'expo-router';
 import { ArrowLeftStroke } from 'src/components/icons';
 import { useNavigationTheme } from 'src/hooks/theme';
@@ -10,7 +10,9 @@ export default function SettingsLayout() {
         <Stack
             screenOptions={{
                 headerTitleAlign: 'center',
-                headerBackButtonDisplayMode: 'minimal',
+                headerTitleStyle: {
+                    fontSize: 16
+                },
                 headerTintColor: navigationTheme.colors.text,
                 headerLeft: ({ tintColor }) => (
                     <Button variant="ghost" size="icon" onPress={() => router.back()}>
@@ -19,7 +21,7 @@ export default function SettingsLayout() {
                 )
             }}
         >
-            <Stack.Screen name="index" options={{ title: 'Settings' }} />
+            <Stack.Screen name="index" options={{ title: 'Setting' }} />
             <Stack.Screen name="dark-mode" options={{ title: 'Dark mode' }} />
         </Stack>
     );
