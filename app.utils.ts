abd7b8071af5693a8cbeb80a75a62d3c2243e227
Will Zhou <iwillzhou@outlook.com>
2025-03-03 20:14:57 +0800

    fix: translate app metadata on android: fix type error

--- app.utils.ts
+++ app.utils.ts
@@ -2,24 +2,24 @@ import 'ts-node/register';
 
 import en from './src/i18n/locales/en.json';
 
-const appName = en.CFBundleDisplayName;
-const appID = 'com.onepixel.netflix';
-const projectId = 'bebdcc56-dc37-4915-a305-1d03b03f700a';
+const appName: string = en.CFBundleDisplayName; // Explicitly define the type
+const appID: string = 'com.onepixel.netflix';
+const projectId: string = 'bebdcc56-dc37-4915-a305-1d03b03f700a';
 
-export const isProduction = process.env.APP_ENV === 'production';
+export const isProduction: boolean = process.env.APP_ENV === 'production';
 
-export const getAppName = () => {
-    const appNameSuffix = isProduction ? '' : `(${process.env.APP_ENV})`;
+export const getAppName = (): string => {
+    const appNameSuffix: string = isProduction ? '' : `(${process.env.APP_ENV})`;
     return `${appName}${appNameSuffix}`;
 };
 
-export const getAppScheme = () => appName;
+export const getAppScheme = (): string => appName;
 
-export const getUniqueIdentifier = () => {
-    const appIDSuffix = isProduction ? '' : `.${process.env.APP_ENV}`;
+export const getUniqueIdentifier = (): string => {
+    const appIDSuffix: string = isProduction ? '' : `.${process.env.APP_ENV}`;
     return `${appID}${appIDSuffix}`;
 };
 
-export const getProjectId = () => projectId;
+export const getProjectId = (): string => projectId;
 
-export const getUpdatesUrl = () => `${process.env.HOSTNAME}/api/manifest?id=${projectId}&channel=${process.env.APP_ENV}`;
+export const getUpdatesUrl = (): string => `${process.env.HOSTNAME}/api/manifest?id=${projectId}&channel=${process.env.APP_ENV}`;
