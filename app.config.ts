60f687383c65bcb1ddac164869425c080b56f389
Will Zhou <iwillzhou@outlook.com>
2026-01-30 15:13:42 +0800

    refactor: update dependencies

--- app.config.ts
+++ app.config.ts
@@ -32,6 +32,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     },
     locales: getLocales(),
     plugins: [
+        'expo-font',
         'expo-router',
         'expo-secure-store',
         'expo-localization',
