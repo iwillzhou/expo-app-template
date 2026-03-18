9ba8ca72fec6285d94f2d4a81b4f3be81bac2177
Will Zhou <iwillzhou@outlook.com>
2026-03-18 17:49:20 +0800

    chore: upgrade @react-navigation/*

--- src/app/(app)/(tabs)/home.tsx
+++ src/app/(app)/(tabs)/home.tsx
@@ -3,11 +3,9 @@ import { Storage } from 'src/utils/storage';
 import { Button, Text } from 'src/components/ui';
 import { useFontScaleStore } from 'src/stores/font-scale';
 import * as Sentry from '@sentry/react-native';
-import { usePostHog } from 'posthog-react-native';
 
 export default function HomeScreen() {
     const { fontScale } = useFontScaleStore();
-    const posthog = usePostHog();
 
     return (
         <View className="flex-1 items-center justify-center bg-background gap-4">
@@ -22,15 +20,6 @@ export default function HomeScreen() {
                 }}
             >
                 <Text>Try Sentry!</Text>
-            </Button>
-            <Button
-                onPress={() => {
-                    posthog.capture('button_pressed', {
-                        button_name: 'Try PostHog!'
-                    });
-                }}
-            >
-                <Text>Try PostHog!</Text>
             </Button>
         </View>
     );
